a076ebbe6330ed4bdcca4d94a4b84430bb8cf7cc
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2017-11-08 23:31:52 +0700

    fix angular ui bootstrap

--- src/app/front/shared/sub-menu/sub-menu.component.ts
+++ src/app/front/shared/sub-menu/sub-menu.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
-  selector: 'sub-menu',
+  selector: 'app-sub-menu',
   templateUrl: './sub-menu.component.html',
   styleUrls: ['./sub-menu.component.css']
 })
