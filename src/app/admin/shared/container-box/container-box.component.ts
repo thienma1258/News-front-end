162f5fec7cce71231f8957d265c082b8f9d785dd
Sieu <sieuvb@gmail.com>
2017-11-17 08:56:40 +0700

    introduction + update layout

--- src/app/admin/shared/container-box/container-box.component.ts
+++ src/app/admin/shared/container-box/container-box.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'content-box',
+  selector: 'container-box',
   templateUrl: './container-box.component.html',
   styleUrls: ['./container-box.component.css']
 })
 export class ContainerBoxComponent implements OnInit {
-  @Input() title = 'TITLE';
+  @Input() title = '';
   @Input() editLink = '';
+  @Input() isShowSeeMore = false;
+  @Input() isShowEditButton = false;
 
   constructor() {
   }
