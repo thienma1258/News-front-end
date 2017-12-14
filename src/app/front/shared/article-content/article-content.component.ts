5fa07e98d2d409aacc052b8676b9997a518043ae
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2017-12-14 17:40:28 +0700

    remove button en-cn, edit button share with real url

--- src/app/front/shared/article-content/article-content.component.ts
+++ src/app/front/shared/article-content/article-content.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FacebookService, InitParams} from 'ngx-facebook';
 import {Article} from '../../../shared/model/article';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-article-content',
@@ -9,8 +10,9 @@ import {Article} from '../../../shared/model/article';
 })
 export class ArticleContentComponent implements OnInit {
   @Input() article: Article;
+  public currentUrl: string;
 
-  constructor(private fb: FacebookService) {
+  constructor(private router: Router, private fb: FacebookService) {
     const initParams: InitParams = {
       appId: '1291648807589933',
       xfbml: true,
@@ -26,6 +28,7 @@ export class ArticleContentComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.currentUrl = this.router.url;
   }
 
 }
