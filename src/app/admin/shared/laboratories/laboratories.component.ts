689518c3dba96fe55a1908be5a7ca7b78ca38e62
Food <cpud1258@gmail.com>
2017-12-21 22:56:21 +0700

    'add-laboratoires'

--- src/app/admin/shared/laboratories/laboratories.component.ts
+++ src/app/admin/shared/laboratories/laboratories.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {LaboratoryServices} from '../../../shared/services/laboratory.service';
 import {Laboratory} from '../../../shared/model/laboratory.model';
+import {Advisor} from '../../../shared/model/advisor';
+import {AdvisorService} from '../../../shared/services/advisor.service';
 @Component({
   selector: 'laboratories',
   templateUrl: './laboratories.component.html',
@@ -8,14 +10,40 @@ import {Laboratory} from '../../../shared/model/laboratory.model';
 })
 export class LaboratoriesComponent implements OnInit {
   public laboratories: Laboratory[]= [];
-  constructor(private laboratoryServices: LaboratoryServices) { }
+  public isAddnew= true;
+  public advisor: Advisor[]= [];
+  public laboratory: any= {
+    'englishTitle': '',
+    'chineseTitle': '',
+    'englishTeacherName': '',
+    'chineseTeacherName': '',
+    'englishResearch': '',
+    'chineseResearch': ''
+  };
+  constructor(private laboratoryServices: LaboratoryServices,
+    private Advisorservices: AdvisorService) { }
 
   ngOnInit() {
+this.loaddata();
+    this.Advisorservices.getAllAdvisors().subscribe(data => {
+console.log(data);
+this.advisor = data['content'];
+console.log(this.advisor);
+    });
+  }
+  loaddata(){
     this.laboratoryServices.getlaboratory().subscribe(data => {
       console.log(data);
       this.laboratories = data['content'];
 
     });
   }
-
+  addnew(){
+    this.laboratoryServices.addnew(this.laboratory).subscribe(data=>{
+      if(data['content']===true){
+       this.loaddata();
+      }
+      console.log(data);
+    });
+  }
 }
--- src/app/shared/services/laboratory.service.ts
+++ src/app/shared/services/laboratory.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
 
 import {AuthService} from '../../admin/shared/auth.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class LaboratoryServices {
-  laboratoryget = "/laboratory/get";
+  laboratoryget = '/laboratory/get';
+  laboratoryadd= '/laboratory/add';
   constructor(private http: HttpClient, private authService: AuthService) {
 
   }
@@ -15,5 +16,18 @@ export class LaboratoryServices {
     return this.http.get(url);
   }
 
-
+  // tslint:disable-next-line:one-line
+  addnew(data){
+    const url = this.authService.apiUrl + this.laboratoryadd;
+    const options = {
+      headers: new HttpHeaders(
+        {
+          'Content-Type': 'application/json',
+          'Authorization': this.authService.getAccessToken()
+        }
+      )
+    };
+    console.log(this.authService.getAccessToken());
+   return  this.http.post(url, data, options );
+  }
 }
